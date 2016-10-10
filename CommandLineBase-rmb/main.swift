a44526ff8ad9cb9eb702137e51bd31390cc9fa56
Natdog18 <rosebailey@Kimberlys-MacBook-Pro-4.local>
2016-10-10 11:58:36 -0400

    initial class files

---
+++ CommandLineBase-rmb/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  CommandLineBase-rmb
+//
+//  Created by Rose Bailey on 10/10/16.
+//  Copyright © 2016 Interapt. All rights reserved.
+//
+
+import Foundation
+
+let interactive = Interactive()
+
+//Run in interactive mode
+interactive.go()
+
+exit(EXIT_SUCCESS)
+
